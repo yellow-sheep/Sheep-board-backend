6c6e486b4dac57b322f7fc9010a5e932ffca144b
ryhoneydew <lucia.yang88@gmail.com>
2019-02-17 14:02:00 -0500

    Add basic query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -32,15 +32,63 @@ const Query = {
   },
 
   boards(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request);
-    return prisma.query.boards(
-      {
-        where: {
-          author: userId
+    //const userId = getUserId(request);
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query
         }
-      },
-      info
-    );
+      ];
+    }
+    return prisma.query.boards(opArgs, info);
+  },
+
+  lists(parent, args, { prisma, request }, info) {
+    //const userId = getUserId(request);
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query
+        }
+      ];
+    }
+    return prisma.query.lists(opArgs, info);
+  },
+
+  cards(parent, args, { prisma, request }, info) {
+    //const userId = getUserId(request);
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query
+        },
+        {
+          tag_contains: args.query
+        },
+        {
+          text_contains: args.query
+        }
+      ];
+    }
+    return prisma.query.lists(opArgs, info);
   }
 };
 
